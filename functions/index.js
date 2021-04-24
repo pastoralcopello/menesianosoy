ed740889c8d88376bfb5700ac2919365e6ee6bc2
Nicolás Acosta <nicacosta@frba.utn.edu.ar>
2021-04-23 23:24:51 -0300

    timezone correction

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions')
 const fetch = require('node-fetch')
 
-exports.todayHTTP = functions.https.onRequest(async (request, response) => {
-	await fetch(apiURL())
+exports.content = functions.https.onRequest(async (request, response) => {
+	const body = request.body
+	const dateString = body.date
+	const timezoneOffset = body.timezoneOffset
+	const date = new Date(dateString).normalizeTZ(timezoneOffset)
+	await fetch(oneDayAPI(date))
 		.then((res) => res.json())
 		.then((json) => {
 			const data = contentData(json)
@@ -19,17 +23,44 @@ function contentData(response) {
 	}
 }
 
-function today() {
-	const today = new Date()
-	return today.toISOString().split('T')[0]
+function oneDayAPI(date) {
+	const origin = dayBefore(date).string()
+	const end = date.string()
+	const url = `http://institutosanpablo.com.ar/aplicacion/wp-json/wp/v2/posts?categories=1&orderby=date&order=desc&after=${origin}T23:59:59&before=${end}T23:59:59`
+	console.log(url)
+	return url
 }
 
-function yesterday() {
-	const yesterday = new Date()
-	yesterday.setDate(yesterday.getDate() - 1)
-	return yesterday.toISOString().split('T')[0]
+function multipleDaysAPI(date, daysBefore, daysAfter) {
+	const origin = date.daysBefore(daysBefore).string()
+	const end = date.daysAfter(daysAfter).string()
+	return `http://institutosanpablo.com.ar/aplicacion/wp-json/wp/v2/posts?categories=1&orderby=date&order=desc&after=${origin}T23:59:59&before=${end}T23:59:59`
 }
 
-function apiURL() {
-	return `http://institutosanpablo.com.ar/aplicacion/wp-json/wp/v2/posts?categories=1&orderby=date&order=desc&after=${yesterday()}T23:59:59&before=${today()}T23:59:59`
+
+Date.prototype.dayBefore = function() {
+	const newDate = new Date(this.getTime());
+	newDate.setDate(this.getDate() - 1);
+	return newDate
+}
+
+Date.prototype.daysBefore = function(days) {
+	const newDate = new Date(this.getTime());
+	newDate.setDate(this.getDate() - days);
+	return newDate
+}
+
+Date.prototype.daysAfter = function(days) {
+	const newDate = new Date(this.getTime());
+	newDate.setDate(this.getDate() + days);
+	return newDate
+}
+
+Date.prototype.string = function() {
+	return this.toISOString().split('T')[0]
+}
+
+Date.prototype.normalizeTZ = function(timezoneOffset) {
+	const newDate = new Date(this.getTime() - timezoneOffset*60000)
+	return newDate
 }
--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,18 @@ const contentDiv = document.querySelector('.content')
 const titleH3 = document.querySelector('.title')
 
 async function loadContent() {
-	const res = await fetch('https://menesianosoy.web.app/todayHTTP')
+	const today = new Date()
+	const timezoneOffset = new Date().getTimezoneOffset()
+	const res = await fetch('https://menesianosoy.web.app/content', {
+	// const res = await fetch('http://localhost:5000/content', {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		method: 'post',
+		body: JSON.stringify({date: today, timezoneOffset: timezoneOffset}),
+	})
 
 	const data = await res.json()
-
-	console.log('httpRequest todayHTTP')
-	console.log(data)
 	contentDiv.innerHTML = data.content
 	titleH3.innerText = data.title
 }
